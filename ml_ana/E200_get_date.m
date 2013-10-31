9534d9808b666aa7a070e50302d76542e278c217
Mike Litos <mikelitos@gmail.com>
2013-10-31 16:44:15 -0700

    added scripts to retrieve date and B5D36 from normal data struct.

---
+++ ml_ana/E200_get_date.m
@@ -0,0 +1,43 @@
+%% E200_get_date
+%  function to get the date in various formats from
+%  the main data struct
+function [ data_date ] = E200_get_date( data, format )
+
+% read in date from filename
+filename = data.VersionInfo.originalfilename;
+
+% tokenize it
+
+% experiment name
+[exp filename] = strtok(filename,'_');
+% dataset number
+[dataset filename] = strtok(filename(2:end),'_');
+% year
+[year filename] = strtok(filename(2:end),'-');
+% month
+[month filename] = strtok(filename(2:end),'-');
+% day
+[day filename] = strtok(filename(2:end),'-');
+% hour
+[hour filename] = strtok(filename(2:end),'-');
+% minute
+[min filename] = strtok(filename(2:end),'-');
+% second
+[sec filename] = strtok(filename(2:end),'-');
+
+if strcmpi(format,'year')
+    data_date = year;
+elseif strcmpi(format,'month')
+    data_date = year;
+elseif strcmpi(format,'day')
+    data_date = year;
+elseif strcmpi(format,'ymd')
+    data_date = [year month day];
+elseif strcmpi(format,'time')
+    data_date = [hour ':' min ':' sec];
+else
+    data_date = [year month day hour min sec];
+end
+
+end
+
